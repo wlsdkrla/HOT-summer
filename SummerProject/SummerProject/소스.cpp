bc645a793907deafb2cee8d6d25ff69d96d508c7
wlsdkrla <wlswlsdk23@naver.com>
2021-06-26 13:23:53 +0900

    pacmen 움직임

--- SummerProject/SummerProject/소스.cpp
+++ SummerProject/SummerProject/소스.cpp
@@ -1,19 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <iostream>
 using namespace sf;
+
 using namespace std;
 int main() {
-	
+
 	RenderWindow window(sf::VideoMode(500, 500), "WINDOW");
-	window.setFramerateLimit(60);
-	
+	window.setFramerateLimit(300);
+
 	vector<Texture>txVector;
 	Texture tx;
 	char name[50];
-	for (int i = 32; i < 36; ++i) {
-		sprintf(name, "Textures/cookie0020x2/cookie0020x2_00%d.png",i);
+	for (int i = 1; i < 7; ++i) {
+		sprintf(name, "Textures/pacman/%d.jpg", i);
 		tx.loadFromFile(name);
 		txVector.push_back(tx);
 	}
@@ -22,9 +23,9 @@ int main() {
 	size_t keyFrameTime = 0;
 	while (window.isOpen()) {
 		window.clear();
-		
+
 		Event e;
-		
+
 		while (window.pollEvent(e)) {
 			switch (e.type)
 			{
@@ -32,20 +33,22 @@ int main() {
 				window.close();
 				break;
 			}
-
 			default:
 				break;
 			}
 		}
-		
-		if (clock.getElapsedTime().asSeconds() > 0.2f) {
-			
+	
+
+	
+		if (clock.getElapsedTime().asSeconds() > 0.5f) {
+
 			sp.setTexture(txVector.data()[keyFrameTime % txVector.size()]);
 			++keyFrameTime;
 			clock.restart();
 		}
+
 		window.draw(sp);
-	
+
 		window.display();
 	}
 }
